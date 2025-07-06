6b8b69c2dcd62efec79c4873ce62f8897582f57b
Yana Sokurenko <77590795+YanaMoss@users.noreply.github.com>
2025-07-06 16:50:42 +0300

    Done

---
+++ mybalance/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div style={{ padding: "2rem", textAlign: "center" }}>
+      <h1>My balance 💙</h1>
+      <p>Сайт в розробці</p>
+    </div>
+  );
+}
+
+export default App;
